9accfa2ca70eba4e9e3142ec1fdb6f711159dcd9
santorol <lenbarbsan@gmail.com>
2019-10-18 15:49:33 -0400

    Removed a line from batch file

--- sortfiles.bat
+++ sortfiles.bat
@@ -1,3 +1,2 @@
-cd C:\Users\leonard.santoro\AppData\Local\Programs\Python\Python37-32\
-python sort_files.py
+python "C://Users//leonard.santoro//AppData//Local//Programs//Python//Python37-32//sort_files.py"
 pause 
