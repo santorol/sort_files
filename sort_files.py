11ddbbb8c49e779e6497385d4f7f558f64de1c04
santorol <lenbarbsan@gmail.com>
2019-12-02 16:33:36 -0500

    fix spacing

--- sort_files.py
+++ sort_files.py
@@ -4,7 +4,10 @@ import os
 import shutil
 from datetime import datetime
 import logging
+import time
+from monthdelta import MonthDelta
 
+logging.basicConfig(filename='cleanfolder.log',level=logging.INFO)
 
 def sort_files_in_a_folder(mypath):
 	'''
@@ -15,8 +18,6 @@ def sort_files_in_a_folder(mypath):
 	
 		now = datetime.now()
 		str_now = now.strftime("%d-%m-%Y")
-		
-		logging.basicConfig(filename='cleanfolder.log',level=logging.DEBUG)
 		
 		files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
@@ -59,10 +60,37 @@ def sort_files_in_a_folder(mypath):
 				else:	
 					shutil.move(src_path,dest_path)
 				print(src_path + '>>>' + dest_path)
+				logging.info(str_now + src_path + '>>>' + dest_path)
 				
 	except Exception as e:
 		logging.error(str_now +"Exception Occured", exc_info=True)
-
+		
+		
+		
+def remove_old_backups(dest):
+	'''Only keep the last 5 backup folders'''
+	str_now = now.strftime("%d-%m-%Y")
+	
+	try:
+		arch_date= now - MonthDelta(1)
+		directories=[os.path.join(dest, name) for name in os.listdir(dest) if os.path.isdir(os.path.join(dest, name))]
+		old_dirs = [name.replace('\\' , '/') for name in directories if os.path.getctime(name) < time.mktime(arch_date.timetuple())]
+		for name in old_dirs:
+			print( 'Removing ' + name)
+			#shutil.rmtree(name,ignore_errors=True)
+			dir_count = 0
+			file_count = 0
+			for _, dirs, files in os.walk(name):
+				dir_count += len(dirs)
+				file_count += len(files)
+			if (dir_count + file_count) == 0:
+				os.rmdir(name)
+		
+	except OSError as e:
+		logging.error(str_now +"Exception Occured", exc_info=True)
+	
+	
+	
 def backup_local_documents_folder(src, dest):
 	'''Function to move document files to the network home drive'''
 	now = datetime.now()
@@ -75,15 +103,19 @@ def backup_local_documents_folder(src, dest):
 			shutil.copytree(src, dest)
 	except shutil.Error as e:
 		print('Directory not copied. Error: %s' % e)
+		logging.error(str_now +"Directory not copied. Error: %s", exc_info=True)
 	except OSError as e:
 		print('Directory not copied. Error: %s' % e)
+		logging.error(str_now +"Directory not copied. Error: %s", exc_info=True)
 
 
 if __name__=="__main__":
 	now = datetime.now()
 	str_now = now.strftime("%d-%m-%Y")
-	import time
+
 	try:
+		remove_old_backups('//dtnas1hq/users/leonard.santoro/documents')
+		'''
 		mypath='//dtnas1hq/users/leonard.santoro'
 		sort_files_in_a_folder(mypath)
 		
@@ -98,11 +130,12 @@ if __name__=="__main__":
 		dest='//dtnas1hq/users/leonard.santoro/documents/local_bkup_'+str_now
 		start=time.time()
 		backup_local_documents_folder(mypath, dest)
-		
-		
+
+		logging.info('It took', time.time()-start,'seconds to backup downloads folder')
 		logging.info(str_now + ' File Cleaner Completed')
+		
 		print('It took', time.time()-start,'seconds to backup downloads folder')
 		print('File Cleaner Completed')
-		
+		'''
 	except Exception as e:
 		logging.error(str_now +'Exception Occured', exc_info=True)
